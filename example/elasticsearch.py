f8d098576fbcc872686bc5a828468f0cc9e4d5e0
Yanglin Zhao <yz1876@gmail.com>
2020-07-23 22:47:52 -0400

    Progress

--- example/elasticsearch.py
+++ example/elasticsearch.py
@@ -1,26 +1,34 @@
+from elasticsearch import Elasticsearch
+
+from django.conf import settings
+
+
 MOVIE_MAPPING = {
     "properties": {
         "id": {"type": "keyword"},
-        "slug": {"type": "keyword"},
         "title": {"type": "text"},
         "description": {"type": "text"},
     }
 }
-
 
 SHOW_MAPPING = {
     "properties": {
         "id": {"type": "keyword"},
-        "slug": {"type": "keyword"},
         "title": {"type": "text"},
         "description": {"type": "text"},
     }
 }
 
 
-def get_movies():
-    pass
-
-
-def get_shows():
-    pass
+def search_media(query):
+    """Helper method to get movies and shows based on a search query
+    """
+    client = Elasticsearch(settings.ELASTICSEARCH_HOST)
+    body = {"query": {"match_all": {}}}
+    body = {
+        "query": {
+            "multi_match": {"query": query, "fields": ["title", "description"]}
+        }
+    }
+    response = client.search(index=["movie", "show"], body=body)
+    return [h["_source"] for h in response["hits"]["hits"]]
--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -1,3 +1,40 @@
+from example.elasticsearch import search_media
+
+
+def index_test_fixtures(es, index_name, data):
+    created = es.index(index=index_name, body=data)
+    assert created["result"] == "created"
+    es.indices.refresh(index_name)
+
+
 class TestElasticsearch:
     def test_elasticsearch(self, elasticsearch):
-        assert elasticsearch
+        # Setup test fixtures
+        index_test_fixtures(
+            elasticsearch,
+            "movie",
+            {
+                "title": "American Psycho",
+                "description": "A wealthy New York City investment banking executive, Patrick Bateman, hides his alternate psychopathic ego from his co-workers and friends as he delves deeper into his violent, hedonistic fantasies.",
+            },
+        )
+        index_test_fixtures(
+            elasticsearch,
+            "show",
+            {
+                "title": "The Wire",
+                "description": "The Wire is an American crime drama television series created and primarily written by author and former police reporter David Simon.",
+            },
+        )
+        index_test_fixtures(
+            elasticsearch,
+            "show",
+            {
+                "title": "Breaking Bad",
+                "description": "Breaking Bad is an American neo-Western crime drama television series created and produced by Vince Gilligan.",
+            },
+        )
+
+        # Make assertions
+        results = search_media("American")
+        assert results == [1]
